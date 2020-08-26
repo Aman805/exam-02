99adfa5ba5c42b677a9c20a49836b7b9f48eda8a
Aman Nur <54576624+Aman805@users.noreply.github.com>
2020-08-26 15:29:59 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+		"use strict";
+
+	jQuery(document).ready(function(){
+
+
+
+	// team-item-carousel slider
+
+	$(".team-item-carousel").owlCarousel({
+	items: 1,
+	loop:true,
+	autoplay:true,
+	nav:false,
+
+
+	});
+
+
+
+
+
+});
+
+
+
+}(jQuery));
+
